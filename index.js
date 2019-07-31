25250ee6ef05451f7d641085067744802e4f38b5
Mark Bergstrom <markbergstrom@gmail.com>
2019-07-31 14:21:19 -0500

    made responsive, added window height adjustment

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 function smoothScroll() {
-    $('nav a').click(function(event) {
+    $('.nav-anchor, .hero-anchor').click(function(event) {
         event.preventDefault();
         let anchorTarget = $(this).attr("href");
         let targetLocation = $(anchorTarget).offset().top;
@@ -9,4 +9,21 @@ function smoothScroll() {
     });
 }
 
-$(smoothScroll);
+function heroHeight() {
+    let windowHeight = $( window ).height();
+    let navHeight = $('nav').height();
+    let heroHeight = windowHeight - navHeight + "px";
+    $('header').css("min-height", heroHeight);
+}
+
+function windowResize() {
+    window.addEventListener("resize", heroHeight);
+}
+
+function loadPortfolio() {
+    smoothScroll();
+    heroHeight();
+    windowResize();
+}
+
+$(loadPortfolio);
