90ede0af600440de4a88601a4bcc64d0c617056c
Mark Bergstrom <markbergstrom@gmail.com>
2019-07-31 23:14:09 -0500

    added down arrow link in hero.  ready for submission

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+//make the jump link smoothly scroll to their respective sections
 function smoothScroll() {
     $('.nav-anchor, .hero-anchor').click(function(event) {
         event.preventDefault();
@@ -9,17 +10,22 @@ function smoothScroll() {
     });
 }
 
+//set the minimum height of the hero to match the height of the user's browser window
+//this ensures that the hero fills the whole screen when the page loads
 function heroHeight() {
-    let windowHeight = $( window ).height();
+    let windowHeight = $(window).height();
     let navHeight = $('nav').height();
-    let heroHeight = windowHeight - navHeight + "px";
-    $('header').css("min-height", heroHeight);
+    let heroHeight = windowHeight - navHeight;
+    let heroHeightText = heroHeight + "px";
+    $('header').css("min-height", heroHeightText);
 }
 
+//adjust the height of the hero if the window is resized after the page is loaded
 function windowResize() {
     window.addEventListener("resize", heroHeight);
 }
 
+//ready
 function loadPortfolio() {
     smoothScroll();
     heroHeight();
