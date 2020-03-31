538027095cd9c0196650581db0f1a9edaf8e43de
Mark Bergstrom <markbergstrom@gmail.com>
2020-03-31 00:33:21 -0500

    added Traction Mission Control and updated styling

--- index.js
+++ index.js
@@ -12,12 +12,23 @@ function smoothScroll() {
 
 //set the minimum height of the hero to match the height of the user's browser window
 //this ensures that the hero fills the whole screen when the page loads
+//Also set the top limit of the sticky headers to match the nav height.
 function heroHeight() {
     let windowHeight = $(window).height();
     let navHeight = $('nav').height();
+    let headingHeight = $('.heading').height();
     let heroHeight = windowHeight - navHeight;
     let heroHeightText = heroHeight + "px";
+    let bigScreen = $('h2').css("padding-top") === '50px';
     $('header').css("min-height", heroHeightText);
+    $('.heading').css("top", navHeight + "px");
+    $('main').css("margin-top", navHeight + "px");
+    $('.pagejump').css("top", -navHeight + "px");
+    $('h3').css("top", navHeight + headingHeight + 'px');
+    if (bigScreen) {
+        $('h2').css("top", navHeight + 'px');
+        $('h3').css("top", navHeight + 'px');
+    }
 }
 
 //adjust the height of the hero if the window is resized after the page is loaded
